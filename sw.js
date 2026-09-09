c558749215e3e2ddcc7c84b0a7ad46a85c9fd9b0
toshiyuki1107 <toshiyuki1736@gmail.com>
2026-09-09 17:12:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'uriba-hyoka-v3';
+const CACHE_NAME = 'uriba-hyoka-v4';
 const ASSETS = ['./売場評価アプリ.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
